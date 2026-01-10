5a4955a9dc24f28e278688076f66078a44b96989
Javaski404 <javaski404@gmail.com>
2026-01-10 19:46:56 +0200

    Add files via upload

---
+++ 4-sum Positive Numbers/index.js
@@ -0,0 +1,11 @@
+function sumPositiveNumbers(arr) {
+  let sum = 0;
+  for (let num of arr) {
+    if (num > 0) {
+      sum += num;
+    }
+  }
+  return sum;
+}
+
+console.log(sumPositiveNumbers([1, -2, 3, 4, -5])); // 8
